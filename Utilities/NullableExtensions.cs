fa53128e25c857588872fc9a7829f450d17440b1
Jan Kytka <kytka.honza@gmail.com>
2022-07-20 09:56:41 +0200

    Migrate to .NET 6

--- Utilities/NullableExtensions.cs
+++ Utilities/NullableExtensions.cs
@@ -22,25 +22,15 @@ namespace IotDash.Utils.Nullables {
 
             var nullable = customAttributes
                 .FirstOrDefault(x => x.AttributeType.FullName == "System.Runtime.CompilerServices.NullableAttribute");
-            if (nullable != null && nullable.ConstructorArguments.Count == 1) {
-                var attributeArgument = nullable.ConstructorArguments[0];
-                if (attributeArgument.ArgumentType == typeof(byte[])) {
-                    var args = (ReadOnlyCollection<CustomAttributeTypedArgument>)attributeArgument.Value!;
-                    if (args.Count > 0 && args[0].ArgumentType == typeof(byte)) {
-                        return (byte)args[0].Value! == 2;
-                    }
-                } else if (attributeArgument.ArgumentType == typeof(byte)) {
-                    return (byte)attributeArgument.Value! == 2;
-                }
+            if (nullable != null) {
+                return true;
             }
 
             for (var type = declaringType; type != null; type = type.DeclaringType) {
                 var context = type.CustomAttributes
                     .FirstOrDefault(x => x.AttributeType.FullName == "System.Runtime.CompilerServices.NullableContextAttribute");
-                if (context != null &&
-                    context.ConstructorArguments.Count == 1 &&
-                    context.ConstructorArguments[0].ArgumentType == typeof(byte)) {
-                    return (byte)context.ConstructorArguments[0].Value! == 2;
+                if (context != null) {
+                    return true;
                 }
             }
 
